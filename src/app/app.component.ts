5e7f8558fed9d0b5b70c9a7e69a9b40ac2e4362b
Zacharey Beatty <zacharey@gmail.com>
2016-08-02 07:56:22 -0600

    Training commit from 8/2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [
+    MD_CARD_DIRECTIVES,
+    MD_SIDENAV_DIRECTIVES,
+    MD_TOOLBAR_DIRECTIVES,
+    MdIcon
+  ],
+  providers: [MdIconRegistry]
 })
 export class AppComponent {
   title = 'app works!';
